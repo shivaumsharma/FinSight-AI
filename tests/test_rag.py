b38ebefff4274e64cb16353055eaed6fedbc7a77
Shivaum Shekhar Sharma <sshivaum@gmail.com>
2026-05-22 23:51:50 +0530

    Built Interactive Streamlit RAG dashboard with semantic retrieval

--- tests/test_rag.py
+++ tests/test_rag.py
@@ -3,20 +3,20 @@ from app.rag.report_generator import(ReportGenerator)
 
 if __name__=="__main__":
   rag_pipeline=(RAGPipeline())
-  transcript_path=("data/apple_q2.txt")
+  transcript_path=("app/data/apple_q2.txt")
   rag_pipeline.ingest_transcript(transcript_path)
   query=("What did the management say about AI demand?")
-  retrived_chunks=(rag_pipeline.query_pipeline(query=query,n_results=3))
+  retrieved_chunks=(rag_pipeline.query_pipeline(query=query,n_results=3))
 
   print("\n===Retrived Chunks===\n")
 
-  for chunk in retrived_chunks:
+  for chunk in retrieved_chunks:
     print(chunk)
     print("\n--------\n")
 
-    report_generator=(ReportGenerator(api_key="YOUR_OPENAI_API_KEY"))
+    # report_generator=(ReportGenerator(api_key="YOUR_OPENAI_API_KEY"))
 
-    generated_response=(report_generator.generate_response(query=query,retrived_chunks=retrived_chunks))
+    # generated_response=(report_generator.generate_response(query=query,retrived_chunks=retrived_chunks))
 
-    print("\n===Generated Response===\n")
-    print(generated_response)
+    # print("\n===Generated Response===\n")
+    # print(generated_response)
